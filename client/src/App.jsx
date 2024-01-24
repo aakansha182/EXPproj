cea1fb70acceabbcbe57d10987fdc9a52f6fd8c3
aakansha182 <155164854+aakansha182@users.noreply.github.com>
2024-01-24 20:55:18 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer.jsx";
+import Home from "./components/Home.jsx";
+import OrderPopup from "./components/OrderPopup/OrderPopup.jsx";
+import Login from "./components/login";
+import Register from "./components/register";
+import ForgotPassword from "./components/forgortpassword";
+import ResetPassword from "./components/reset_password";
+import TestFile from "./components/TestFile.jsx";
+import UserProfile from "./components/Userprofile/Userprofile.jsx";
+import AllBooks from "./components/AllBooks/AllBooks.jsx";
+
+const App = () => {
+  const [user, setLoginUser] = useState({});
+
+  const [orderPopup, setOrderPopup] = useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar
+        setLoginUser={setLoginUser}
+        handleOrderPopup={handleOrderPopup}
+      
+    
+        AllBooks={
+          user && user._id ? (
+            <a
+              href={"/allbooks"}
+              className="bg-gradient-to-r cursor-pointer from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3"
+            >
+              Books
+            </a>
+          ) : (
+            ""
+          )
+        }
+
+        logOut={
+          user && user._id ? (
+            <a
+              href={"/login"}
+              onClick={() => setLoginUser({})}
+              className="bg-gradient-to-r cursor-pointer from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3"
+            >
+              Logout
+            </a>
+          ) : (
+            ""
+          )
+        }
+      />
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              user && user._id ? (
+                <Home handleOrderPopup={handleOrderPopup} />
+              ) : (
+                <Login setLoginUser={setLoginUser} />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoginUser={setLoginUser} />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgortpassword" element={<ForgotPassword />} />
+          <Route
+            path="/reset_password/:id/:token"
+            element={<ResetPassword />}
+          />
+          <Route path="/userprofile" element={<UserProfile/>}/>
+          <Route path="/allbooks" element={<AllBooks/>}/>
+          <Route path="/test" element={<TestFile />} />
+        </Routes>
+      </Router>
+
+      <Footer />
+      <OrderPopup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,236 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+
+import multer from "multer";
+import { promisify } from "util";
+import { writeFile } from "fs/promises";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// {
+//   origin: ["https://explore-me.vercel.app"],
+//   methods: ["POST", "GET"],
+//   credentials: true,
+// }
+
+mongoose.connect(
+  // process.env.MONGO_URL,
+  //exp-proj in mongo
+  "mongodb+srv://exp:exp@expcluster.r3892ua.mongodb.net/expdb?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("\nDB connected");
+  }
+);
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      unique: true,
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    email: {
+      unique: true,
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = new mongoose.model("User", userSchema);
+
+//Routes
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "Login Successfull", user: user });
+      } else {
+        res.send({ message: "Password didn't match" });
+      }
+    } else {
+      res.send({ message: "User not registered", error: err });
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  const { name, username, gender, dob, email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "User already registerd" });
+    } else {
+      const user = new User({
+        name,
+        username,
+        gender,
+        dob,
+        email,
+        password,
+      });
+      user.save((err) => {
+        if (err) {
+          res.send({ error: err });
+        } else {
+          res.send({ message: "Successfully Registered, Please login now." });
+        }
+      });
+    }
+  });
+});
+
+app.post("/forgortpassword", async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    console.error("Error finding user:");
+    return res.status(404).json({ Status: "User not existed!" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "jwt_secret_key", {
+    expiresIn: "1d",
+  });
+
+  const url = `http://localhost:5173/reset_password/${user._id}/${token}`;
+  const emailHtml = `<h2>Click to reset password : ${url}</h2>`;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: true,
+    auth: {
+      user: "it24img@gmail.com",
+      pass: "fvfh msrl wuru ulkq",
+    },
+  });
+
+  const options = {
+    from: "it24img@gmail.com",
+    to: email,
+    subject: "Explore - Reset Password",
+    html: emailHtml,
+  };
+
+  const emailSender = await transporter.sendMail(options);
+
+  res.send({ message: "Check your email", user: user, data: emailSender });
+});
+
+app.post("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+
+  jwt.verify(token, "jwt_secret_key", async (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+    try {
+      const userExists = await User.findOne({ _id: id });
+      if (!userExists) {
+        return res.send({ message: "Invalid token or ID" });
+      }
+      userExists.password = password;
+
+      await userExists.save();
+      res.send({ message: "Password Reset done" });
+    } catch (error) {
+      return res.send({ error: error });
+    }
+  });
+});
+
+const uploadImg = new mongoose.Schema(
+  {
+    email: {
+      unique: true,
+      type: String,
+      required: true,
+    },
+    imgPath: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const UploadImg = new mongoose.model("UploadImg", uploadImg);
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("profileImg"), async (req, res) => {
+  // const data = await req.formData();
+
+  try {
+    const email = req.body.email;
+    const profileImg = req.file;
+
+    let profileImgPath = "";
+
+    const user = await UploadImg.findOne({ email: email });
+    if (user) {
+      return res.send({ message: "Use another email", user: user });
+    }
+
+    if (profileImg) {
+      const bufferProfile = profileImg.buffer;
+      const profileImgPathPublic = `../client/public/users/profiles/${
+        email + "_" + profileImg.originalname
+      }`;
+      await writeFile(profileImgPathPublic, bufferProfile);
+      profileImgPath = `/users/profiles/${
+        email + "_" + profileImg.originalname
+      }`;
+    } else {
+      profileImgPath = "noProfile";
+    }
+
+    const uploadImgData = new UploadImg({
+      email,
+      imgPath: profileImgPath,
+    });
+    await uploadImgData.save();
+
+    res.send({ message: "Uploaded img successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("BE started at port 3001");
+});
